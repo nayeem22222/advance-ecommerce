c5404abf49dc7ba89828a9be3235d3d2188b1188
nayeem22222 <dsnayeem@gmail.com>
2021-11-04 00:33:01 +0600

    Upload Slider and Show All Slider List Part 1

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ProductController;
+use App\Http\Controllers\Backend\SliderController;
 
 use App\Http\Controllers\Frontend\IndexController;
 use App\Models\User;
@@ -168,3 +169,11 @@ Route::prefix('product')->group(function(){
      
     
     });
+
+// Admin Slider All Routes 
+
+Route::prefix('slider')->group(function(){
+
+    Route::get('/view', [SliderController::class, 'SliderView'])->name('manage-slider');
+
+});
