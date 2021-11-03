b90219c80b5947a0f405c17e221e96f31c35f82f
nayeem22222 <dsnayeem@gmail.com>
2021-11-04 01:08:14 +0600

    Upload Slider and Show All Slider List Part 2

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -15,4 +15,35 @@ class SliderController extends Controller
 		$sliders = Slider::latest()->get();
 		return view('backend.slider.slider_view',compact('sliders'));
 	}
+
+    public function SliderStore(Request $request){
+
+    	$request->validate([
+    		 
+    		'slider_img' => 'required',
+    	],[
+    		'slider_img.required' => 'Plz Select One Image',
+    		 
+    	]);
+
+    	$image = $request->file('slider_img');
+    	$name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+    	Image::make($image)->resize(870,370)->save('upload/slider/'.$name_gen);
+    	$save_url = 'upload/slider/'.$name_gen;
+
+	Slider::insert([
+		'title' => $request->title,
+		'description' => $request->description,
+		'slider_img' => $save_url,
+
+    	]);
+
+	    $notification = array(
+			'message' => 'Slider Inserted Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->back()->with($notification);
+
+    } // end method
 }
