bc20d00c33aed1f351ae09bfa7ff338739eb566d
nayeem22222 <dsnayeem@gmail.com>
2021-11-06 01:08:11 +0600

    Slider Edit & Update

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -11,11 +11,13 @@ use Image;
 
 class SliderController extends Controller
 {
+	//<!--   ------------ View Slider Page -------- -->
     public function SliderView(){
 		$sliders = Slider::latest()->get();
 		return view('backend.slider.slider_view',compact('sliders'));
 	}
 
+	//<!--   ------------ Store Slider Page -------- -->
     public function SliderStore(Request $request){
 
     	$request->validate([
@@ -46,4 +48,57 @@ class SliderController extends Controller
 		return redirect()->back()->with($notification);
 
     } // end method
+
+	//<!--   ------------ Edit Slider Page -------- -->
+	public function SliderEdit($id){
+		$sliders = Slider::findOrFail($id);
+		return view('backend.slider.slider_edit',compact('sliders'));
+	}
+
+	//<!--   ------------ Update Slider Page -------- -->
+	public function SliderUpdate(Request $request){
+    	
+    	$slider_id = $request->id;
+    	$old_img = $request->old_image;
+
+    	if ($request->file('slider_img')) {
+
+    	unlink($old_img);
+    	$image = $request->file('slider_img');
+    	$name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+    	Image::make($image)->resize(870,370)->save('upload/slider/'.$name_gen);
+    	$save_url = 'upload/slider/'.$name_gen;
+
+	Slider::findOrFail($slider_id)->update([
+		'title' => $request->title,
+		'description' => $request->description,
+		'slider_img' => $save_url,
+
+    	]);
+
+	    $notification = array(
+			'message' => 'Slider Updated Successfully',
+			'alert-type' => 'info'
+		);
+
+		return redirect()->route('manage-slider')->with($notification);
+
+    	}else{
+
+    	Slider::findOrFail($slider_id)->update([
+		'title' => $request->title,
+		'description' => $request->description,
+		
+
+    	]);
+
+	    $notification = array(
+			'message' => 'Slider Updated Without Image Successfully',
+			'alert-type' => 'info'
+		);
+
+		return redirect()->route('manage-slider')->with($notification);
+
+    	} // end else 
+    } // end method 
 }
